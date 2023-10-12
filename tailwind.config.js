905d8d89616ddb15775ef28f9b98344455c244ed
RobinSharma-05 <robin9253370222@gmail.com>
2023-10-12 14:06:58 +0530

    configuration-part

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "25px",
+        "2xl": "6rem",
+      },
+    },
+    fontFamily: {
+      Riviera: ["Riviera Nights Trial", "sans-serif"],
+      Jakarta: ["Plus Jakarta Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "light-brown": "#EEE4DB",
+        "light-black": "#252525",
+      },
+    },
   },
   plugins: [],
 };
