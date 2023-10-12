54564bc0b4e53a4b6f2c68a35609bfe4ffbb1e56
Dipakkkk <dr916139@gmail.com>
2023-10-12 14:10:37 +0530

    first-dev

---
+++ src/components/Icons.jsx
@@ -0,0 +1,57 @@
+export const Real_left_arrow = () => {
+    return (
+      <>
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M8.33325 4.16668L2.49992 10L8.33325 15.8333"
+            stroke="#FCFCFA"
+            stroke-width="0.833333"
+            stroke-miterlimit="10"
+          />
+          <path
+            d="M2.49992 10L18.3333 10"
+            stroke="#FCFCFA"
+            stroke-width="0.833333"
+            stroke-miterlimit="10"
+          />
+        </svg>
+      </>
+    );
+}
+export const Real_right_arrow = () => {
+    return (
+      <>
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.6667 15.8333L17.5001 10L11.6667 4.16667"
+            stroke="#FCFCFA"
+            stroke-width="0.833333"
+            stroke-miterlimit="10"
+          />
+          <path
+            d="M17.5001 10H1.66675"
+            stroke="#FCFCFA"
+            stroke-width="0.833333"
+            stroke-miterlimit="10"
+          />
+        </svg>
+      </>
+    );
+}
+// export const  = () => {
+//     return (
+
+//     )
+// }
---
+++ src/components/Real5.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Real5 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Real5
